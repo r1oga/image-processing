4af899230b34b8ff4565015dd29a891bb8b9e3c7
r1oga <38692952+r1oga@users.noreply.github.com>
2022-09-29 11:23:24 +0200

    configure wasm webpack plugin

--- public/main.js
+++ public/main.js
@@ -1,1 +1,33 @@
-console.log('test')
+let rustApp = null
+
+async function loadRustApp() {
+  try {
+    import('../pkg')
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+async function init() {
+  await loadRustApp()
+  console.log(rustApp)
+  const input = document.getElementById('upload')
+
+  // using filereader to transfer strings between JS and Rust
+  // easier to transfer strings than binary data across languages
+  const fileReader = new FileReader()
+
+  input.addEventListener('change', () => {
+    fileReader.readAsDataURL(input.files[0])
+  })
+
+  fileReader.onloadend = () => {
+    const base64 = fileReader.result.replace(
+      /^data:image\/(png|jpeg|jpg);base64,/,
+      ''
+    )
+    console.log({ base64, raw: input.files[0] })
+  }
+}
+
+init()
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const WasmPackPlugin = require('@wasm-tool/wasm-pack-plugin')
 
 module.exports = {
   entry: './public/main.js',
@@ -7,5 +8,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js'
   },
-  plugins: [new HTMLWebpackPlugin({ template: './public/index.html' })]
+  plugins: [
+    new HTMLWebpackPlugin({ template: './public/index.html' }),
+    new WasmPackPlugin({ crateDirectory: path.resolve(__dirname, '.') })
+  ]
 }
